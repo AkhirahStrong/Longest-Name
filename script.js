8884b14045f09f4af4a8d25cdcc0b44772aa4d3d
Akhirahstrong <codeclubdetroit@gmail.com>
2023-04-24 04:30:37 -0400

    Iteration 2

--- script.js
+++ script.js
@@ -3,16 +3,27 @@ let name2 = "Rihanna Fenty";
 let name3 = "Eric Bishop";
 
 let longestName = "";
-let tiedName = "";
+let tiedName = [];
 
 if (name1.length > longestName.length) {
   longestName = name1;
-} else if (name2.length > longestName.length) {
+  tiedName = [name1];
+} else if (name1.length === longestName.length) {
+  tiedName.push(name1);
+}
+
+if (name2.length > longestName.length) {
   longestName = name2;
-} else if (name3.length > longestName.length) {
+  tiedName = [name2];
+} else if (name2.length === longestName.length) {
+  tiedName.push(name2);
+}
+
+if (name3.length > longestName.length) {
   longestName = name3;
-} else if (longestName == name1 && longestName == name2) {
-  (tiedName = name1), name2;
+  tiedName = [name3];
+} else if (name3.length === longestName.length) {
+  tiedName.push(name3);
 }
 
 console.log("The longest name of three names are both: " + tiedName);
